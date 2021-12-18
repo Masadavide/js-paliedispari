9f10ff9e79937e90c6a65ce7f4d0691584522d07
Masadavide <davide.masadm@gmail.com>
2021-12-18 16:54:38 +0100

    finito

--- palindromi/myscript.js
+++ palindromi/myscript.js
@@ -3,3 +3,30 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
 console.log("esercizio 1")
+
+function checkPalindrome(parola) {
+    let lunghezza = parolaD.length;
+    for(let i = 0; i < lunghezza / 2; i++) {
+        if(parolaD[i] == parolaD[lunghezza - 1 - i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let parola = prompt('Inserisci una parola:');
+let parolaD = [];
+
+
+for(i=0; i<parola.length; i++){
+    parolaD.push(parola[i]);
+    console.log(parola[i]);
+}
+console.log(parolaD);
+
+
+if (checkPalindrome(parola)) {
+    console.log("La parola è palindroma")
+}else {
+    console.log("La parola non è palindroma")
+}
